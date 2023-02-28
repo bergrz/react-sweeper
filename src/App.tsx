c98c26b05774a5741158f457efff4989591f0a0d
Grzegorz Berlinski <124539953+bergrz@users.noreply.github.com>
2023-02-28 15:28:04 +0100

    feat: Add simple state

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
+import { Board } from "./components/Board";
+import { Controls } from "./components/Controls";
 import { Header } from "./components/Header";
 import { config } from "./config";
+import { TBoardState } from "./types";
 
 export default function App() {
-  return <Header headerConfig={config.header} />;
+  const [boardState, setBoardState] = useState<TBoardState>({
+    matrixSize: 3,
+    bombsAmount: 1,
+  });
+  return (
+    <>
+      <Header headerConfig={config.header} />
+      <Controls setBoardState={setBoardState} />
+      <Board {...boardState} />
+    </>
+  );
 }
