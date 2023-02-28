5a5df779e4c69f30a538587763c243950a8a63e4
Grzegorz Berlinski <124539953+bergrz@users.noreply.github.com>
2023-02-28 13:09:04 +0100

    feat: Add project config

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+export const config = {
+  header: {
+    title: "React Sweeper",
+    subtitle:
+      "A simple React application for generating a matrix and drawing bombs on it.",
+  },
+  controls: {
+    matrix: {
+      maxSize: 64,
+    },
+  },
+};
